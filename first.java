64870c8ba39ea6c9788ce556ffc3e02bfbcee0c8
AbhiMejo <abhishek.mejo@gmail.com>
2024-02-09 16:19:29 +0530

    Initial commit

--- first.java
+++ first.java
@@ -1,6 +1,9 @@
+import java.util.Date;
+
 class first{
     public static void main(String[] args) {
         System.out.println("Hello!!!!");
         System.out.println(new Date());
+        System.out.println("Hope you are fine!!!");
     }
 }
